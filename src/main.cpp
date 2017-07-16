7ade9331413743a5c6ecce4aaa61aa7d08a541b7
zlatanajanovic <ajanovic.zlatan@gmail.com>
2017-07-16 21:39:20 +0200

    up

--- src/main.cpp
+++ src/main.cpp
@@ -132,8 +132,8 @@ int main() {
 		  // predict state in 100ms
 		  psi = psi - v*delta/Lf*latency;
 		  v = v + acceleration*latency;
-		  px = px + v*cos(psi)*latency;
-		  py = py + v*sin(psi)*latency;
+		  px = 0 + v*cos(-delta)*latency;
+		  py = 0 + v*sin(-delta)*latency;
 
 		  state << px, py, psi, v, cte, epsi;
 		  
