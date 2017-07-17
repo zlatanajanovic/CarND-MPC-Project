edff0d2e57c1e192a7761bd5272ebba192edbd87
zlatanajanovic <ajanovic.zlatan@gmail.com>
2017-07-17 14:35:13 +0200

    up

--- src/main.cpp
+++ src/main.cpp
@@ -124,14 +124,14 @@ int main() {
 		  // Latency compensation by predicting state
           psi = -v * 0.44704 / Lf * delta * latency;
           px = v * 0.44704 * cos(psi)* latency;
-          py = v * 0.44704* sin(psi)* latency;
+          py = v * 0.44704 * sin(psi)* latency;
 		  
 		  // The cross track error is calculated by evaluating at polynomial at x, f(x)
 		  // and subtracting y.
 		  double cte = polyeval(coeffs, px) - py;//polyeval(coeffs, 0) + v * 0.44704 * sin(- atan(coeffs[1])) * latency; //
 		  // Due to the sign starting at 0, the orientation error is -f'(x).
 		  // derivative of coeffs[0] + coeffs[1] * x -> coeffs[1]
-		  double epsi = - atan(coeffs[1])+psi;
+		  double epsi = - atan(coeffs[1]);//+psi;
 		  v = v + throttle * latency;
 		  
 		  std::cout << "px    " << px << std::endl;
